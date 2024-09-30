746ff4ec95441ddd3e3c233584f6728b2c188ddb
Ankiit <ankit8382040259@gmail.com>
2024-09-30 12:29:13 +0530

    new commit message

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import './DescriptionBox.css'
 const  DescriptionBox = ()=>{
     return ( 
-        <div></div>
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+               <p>
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, atque! Accusamus voluptates expedita deleniti laudantium ab id corporis illum laborum quaerat ducimus temporibus itaque odio esse, aspernatur sequi officia eligendi dolor eos, assumenda voluptate aliquam necessitatibus illo cumque? Possimus tempore quasi obcaecati atque! Reprehenderit amet nisi quae minima, quisquam illo tempora quibusdam magni ad omnis doloremque fugiat perferendis facere provident aut animi placeat non tempore debitis earum unde voluptates sapiente ab dicta? Nobis recusandae 
+                
+               </p>
+               <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum deserunt culpa nesciunt earum vel blanditiis, debitis veniam maiores, quo dicta minima quidem quasi sequi! Id, maxime omnis non, ipsa, ducimus dolores provident magnam assumenda quod est distinctio aperiam. Recusandae delectus, mollitia numquam ut natus magni enim quaerat laudantium animi reiciendis illo non officiis, impedit quidem ullam tenetur, nisi quae fugit consequuntur atque vero voluptate 
+               </p>
+            </div>
+
+        </div>
      );
 }
 
